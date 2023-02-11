7001573a1745482d0eb13326bcf3b3d7ff7059a9
MikeAde07 <MikeAde07@gmail.com>
2023-02-10 23:08:00 -0600

    Gameplay improvement

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,9 +19,9 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    //Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
 
+    while(enemyHealth > 0) {
+        
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."); 
 
     // if player choses to fight, then fight
@@ -73,14 +73,14 @@ var fight = function(enemyName) {
         window.alert("You need to choose a valid option. Try again!");
     }
 
+}
     
-
-
-
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
 
 
