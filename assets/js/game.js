bc879a1b6762b2fe94e1c7336368e805007b62a4
MikeAde07 <MikeAde07@gmail.com>
2023-02-21 20:06:39 -0600

    blank-null-mixed user case input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,11 @@
 
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
 
+    return value;
+}
 
 
 
@@ -72,14 +77,14 @@ var fight = function(enemy) {
 // function to start a new game
 var startGame = function() {
 
-for(var i = 0; i < enemyInfo.length; i++) {
-    
     // reset player stats
     playerInfo.reset();
-
+for(var i = 0; i < enemyInfo.length; i++) {
+    // if player is still alive, keep fight next enemy
     if(playerInfo.health > 0) {
         //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        
 
         //pick new enemy to fight based on the index of the enemyInfo array
         var pickedEnemyObj = enemyInfo[i];
@@ -170,16 +175,23 @@ var shop = function() {
 
 }
 
-// function to generate a random numeric value
-var randomNumber = function(min, max) {
-    var value = Math.floor(Math.random() * (max - min + 1) + min);
+//function to set name
+var getPlayerName = function() {
+    var name = "";
 
-    return value;
+    //Add loop with prompt and condition
+    while(name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
 }
 
 
+
 var playerInfo = {
-    name : window.prompt("What is your robot's name?"),
+    name : getPlayerName(),
     health : 100,
     attack : 10,
     money : 10,
